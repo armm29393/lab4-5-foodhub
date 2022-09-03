2d781af76f6f88d1d7190c26a701480639b42f4a
Pawarit Thong-ek <arm.techhitz@gmail.com>
2022-09-04 00:08:51 +0700

    remove unused

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-{/* <li class="list-group-item">TEST</li> */}
 const foods = {
   1: {
     name: 'ข้าวหน้าหมูไข่ดอง',
@@ -14,34 +13,7 @@ const foods = {
   },
 }
 
-// const setFoods = (id, increment) => {
-//   console.log('food id ==', id)
-//   console.log('food ==', foods[id])
-//   return foods[id] + 1
-// }
-
 const addOrder = (id) => {
-  // let orderList = $("#orderList");
-  // orderList.empty()
-  // // ===== 1 =====
-  // let item1 = document.createElement('li')
-  // item1.setAttribute('class', 'list-group-item')
-  // item1.appendChild(document.createTextNode("ข้าวหน้าหมูไข่ดอง"))
-  // item1.appendChild(document.createTextNode(`      x ${setFoods(1, (id==1)?1:0)}`))
-  // // ===== 2 =====
-  // let item2 = document.createElement('li')
-  // item2.setAttribute('class', 'list-group-item')
-  // item2.appendChild(document.createTextNode("แซลมอน"))
-  // item2.appendChild(document.createTextNode(`      x ${setFoods(2, (id==2)?1:0)}`))
-  // // ===== 3 =====
-  // let item3 = document.createElement('li')
-  // item3.setAttribute('class', 'list-group-item')
-  // item3.appendChild(document.createTextNode("ไอศกรีม"))
-  // item3.appendChild(document.createTextNode(`      x ${setFoods(3, (id==3)?1:0)}`))
-  // orderList.append(item1)
-  // orderList.append(item2)
-  // orderList.append(item3)
-  
   let orderList = $("#orderList");
   let item = document.createElement('li')
   item.setAttribute('class', 'list-group-item')
